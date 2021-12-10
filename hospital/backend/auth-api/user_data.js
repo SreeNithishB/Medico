673f3861ba2e88d05ed8cd0d0156cdab02c71eaf
Sree Nithish B <56504657+SreeNithishB@users.noreply.github.com>
2021-12-11 01:03:56 +0530

    Add files via upload

---
+++ hospital/backend/auth-api/user_data.js
@@ -0,0 +1,43 @@
+// static user details
+
+let userData = [
+
+    {
+    userId: "789789",
+    password: "123456",
+    name: "Sree Nivetha B B",
+    username: "sree_nivetha_bb",
+    isAdmin: true
+
+    },
+  
+    {
+    userId: "789789",
+    password: "123456",
+    name: "Jupuru Harshith",
+    username: "harshith",
+    isAdmin: true
+
+    },
+
+    {
+    userId: "789789",
+    password: "123456",
+    name: "Sree Nithish B",
+    username: "sree_nithish_b",
+    isAdmin: true
+
+    },
+
+    {
+    userId: "789789",
+    password: "123456",
+    name: "Sandeep Thadem",
+    username: "sandeep",
+    isAdmin: true
+
+    },
+  ];
+
+
+module.exports = { userData : userData };
